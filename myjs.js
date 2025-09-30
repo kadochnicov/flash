5113a7af41e376f15cf05e3ff3c0f349cca1db01
Andrew <kadochnikovandrew@gmail.com>
2025-10-01 00:45:56 +0300

    add file second.js

--- myjs.js
+++ myjs.js
@@ -1,19 +1,11 @@
 //Complete the function so that it finds the average of the three scores passed to it and returns the letter value associated with that grade.
-
 function getGrade (s1, s2, s3) {
-    let avg = (s1 + s2 + s3) / 3;
-    if (avg >= 0 || avg < 60) {
-        return 'F';
-    } else if (avg >= 60 || avg < 70) {
-        return 'D';
-    } else if (avg >= 70 || avg < 80) {
-        return 'C';
-    } else if (avg >= 80 || avg < 90) {
-        return 'B';
-    } else if (avg >= 90 || avg < 100) {
-        return 'A';
-    }
-  } 
-
+    avg = (s1+s2+s3)/3;
+    if (avg < 60)  return "F";
+      else if (avg < 70) return "D";
+      else if (avg < 80) return "C";
+      else if (avg < 90) return "B";
+      else return "A";
+  }
 
 getGrade(0,40,60)
