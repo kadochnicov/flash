b588f4e51cf3a8479364b39de934317b839f8115
Andrew <kadochnikovandrew@gmail.com>
2025-10-03 00:45:41 +0300

    накостылил бэтман функцию

--- batman.js
+++ batman.js
@@ -1,3 +1,18 @@
+let getQuotes = function(quotes, hero) {
+  function findPerson(value){
+    return value == '1' || value == '2' || value == '0'
+  }
+
+  let personIndex = hero.split('').filter(findPerson);
+
+  let person = '';
+
+  if (personIndex == '1') {person = 'Rob1n'}
+  if (personIndex == '2') {person = 'Rob2n'}
+  console.log(person + ":" + quotes[+personIndex])
+
+}
+
 let quotes = [
   "I am vengeance. I am the night. I am Batman!",
   "Holy haberdashery, Batman!",
@@ -5,3 +20,5 @@ let quotes = [
 ]
 
 let hero = "Rob1n" 
+
+getQuotes (quotes, hero);
