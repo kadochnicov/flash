cd9be74289922d070f0754a3a4c7c319b628ebd1
kdchnkv <kdchnkv@yandex.by>
2025-11-15 18:00:28 +0300

    add some objects listneners

--- main.js
+++ main.js
@@ -3,7 +3,13 @@ let block = document.querySelector('.blc_for_hide');
 
 function togle_visabilty_block () {
     block.classList.toggle('hide');
-    console.log('sd')
+
+}
+
+function showEvent(event){
+    console.log(event.target);
 }
 
 btn.addEventListener('click', togle_visabilty_block);
+btn.addEventListener('click', showEvent)
+block.addEventListener('mouseover', showEvent)
