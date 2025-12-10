cc8167538e7e690d5323573d18906141d13d985d
Andrew <kadochnikovandrew@gmail.com>
2025-12-10 23:46:26 +0300

    add close element

--- todoServer/app.js
+++ todoServer/app.js
@@ -1,5 +1,6 @@
 //Global data
 let list = document.querySelector('#todo-list');
+let dropdown = document.querySelector('#user-todo');
 let users = [];
 let todos= [];
 
@@ -15,17 +16,29 @@ function getName(userId){
 function printTodos({id, userId, title, completed}) {
     let li = document.createElement('li');
     li.innerHTML = `<span>${title} by ${getName(userId)}</span>`;
+    li.classList.add('todo-item');
+    li.dataset.id = id;
 
     let status = document.createElement('input');
     status.type = 'checkbox';
     status.checked = completed;
 
-    li.classList.add('todo-item');
-    li.dataset.id = id;
+    let close = document.createElement('span');
+    close.innerHTML = '&times;';
+    close.classList.add('close');
 
+
+    li.append(close);
     li.prepend(status);
-
     list.prepend(li);
+}
+
+function addUserToDropdawn(user) {
+    let userItem = document.createElement('option');
+    userItem.value = user.id;
+    userItem.innerText = user.username;
+
+    dropdown.append(userItem);
 }
 
 //event logic
@@ -34,7 +47,8 @@ function appInit(){
         .then((inf)=>{
             [users, todos] = inf;
             
-            todos.forEach(element =>printTodos(element));
+            todos.forEach(element => printTodos(element));
+            users.forEach(element => addUserToDropdawn(element));
         })
 
     //render
